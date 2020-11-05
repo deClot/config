fbcc892c94f4ac7c18fd48ce9d255abe76bda235
Nadezhda Kashirina <refenement@mail.ru>
2020-11-05 12:59:17 +0700

    Ini comit

---
+++ .emacs.d/elpa/helm-pydoc-0.7/helm-pydoc-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-pydoc" "0.7" "pydoc with helm interface"
+  '((helm-core "1.7.4")
+    (cl-lib "0.5"))
+  :commit "30f1814b5b16db0413ffe74b0d0420b38e153df9" :authors
+  '(("Syohei YOSHIDA" . "syohex@gmail.com"))
+  :maintainer
+  '("Syohei YOSHIDA" . "syohex@gmail.com")
+  :url "https://github.com/syohex/emacs-helm-pydoc")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
